5bfe55e61062eadba21b0c6a1807ca619a69d159
Antonio <rssntn67@yahoo.it>
2024-03-08 09:41:59 +0100

    First commit of Cisco UCS Plugin
    Code from archetype

---
+++ plugin/src/main/java/it/xeniaprogetti/cisco/ucs/plugin/shell/StatsCommand.java
@@ -0,0 +1,31 @@
+package it.xeniaprogetti.cisco.ucs.plugin.shell;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import it.xeniaprogetti.cisco.ucs.plugin.AlarmForwarder;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+
+@Command(scope = "opennms-cisco-ucs", name = "stats", description = "Show statistics.")
+@Service
+public class StatsCommand implements Action {
+
+    @Reference
+    private AlarmForwarder forwarder;
+
+    @Override
+    public Object execute() {
+        final MetricRegistry metrics = forwarder.getMetrics();
+        final ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.report();
+        return null;
+    }
+}
