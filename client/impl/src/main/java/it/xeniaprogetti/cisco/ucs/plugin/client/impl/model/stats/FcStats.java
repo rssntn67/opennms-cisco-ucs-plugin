2032ffa2195f3cf1dc6e85e1fb07c6d66f04e32e
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2024-05-17 06:45:07 +0200

    Added FcStats

---
+++ client/impl/src/main/java/it/xeniaprogetti/cisco/ucs/plugin/client/impl/model/stats/FcStats.java
@@ -0,0 +1,47 @@
+package it.xeniaprogetti.cisco.ucs.plugin.client.impl.model.stats;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+public class FcStats extends Stats {
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesRx;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesRxDelta;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesRxDeltaAvg;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesRxDeltaMax;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesRxDeltaMin;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesTx;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesTxDelta;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesTxDeltaAvg;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesTxDeltaMax;
+    @JacksonXmlProperty(isAttribute = true)
+    public long bytesTxDeltaMin;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsRx;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsRxDelta;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsRxDeltaAvg;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsRxDeltaMax;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsRxDeltaMin;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsTx;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsTxDelta;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsTxDeltaAvg;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsTxDeltaMax;
+    @JacksonXmlProperty(isAttribute = true)
+    public long packetsTxDeltaMin;
+}
+
