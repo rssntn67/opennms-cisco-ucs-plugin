dd8042c3622a0783e5f07549550f8c916aa8a5a7
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2024-05-24 20:40:23 +0200

    Partial implementation of collectors

--- plugin/src/main/java/it/xeniaprogetti/cisco/ucs/plugin/collection/compute/UcsComputeCollector.java
+++ plugin/src/main/java/it/xeniaprogetti/cisco/ucs/plugin/collection/compute/UcsComputeCollector.java
@@ -3,13 +3,21 @@ package it.xeniaprogetti.cisco.ucs.plugin.collection.compute;
 import it.xeniaprogetti.cisco.ucs.plugin.client.ClientManager;
 import it.xeniaprogetti.cisco.ucs.plugin.client.api.ApiClientService;
 import it.xeniaprogetti.cisco.ucs.plugin.client.api.ApiException;
+import it.xeniaprogetti.cisco.ucs.plugin.client.api.UcsComputeStats;
 import it.xeniaprogetti.cisco.ucs.plugin.client.api.UcsEnums;
 import it.xeniaprogetti.cisco.ucs.plugin.client.api.UcsUtils;
 import it.xeniaprogetti.cisco.ucs.plugin.collection.AbstractUcsServiceCollector;
 import it.xeniaprogetti.cisco.ucs.plugin.connection.ConnectionManager;
 import org.opennms.integration.api.v1.collectors.CollectionRequest;
 import org.opennms.integration.api.v1.collectors.CollectionSet;
+import org.opennms.integration.api.v1.collectors.resource.NodeResource;
+import org.opennms.integration.api.v1.collectors.resource.immutables.ImmutableCollectionSet;
+import org.opennms.integration.api.v1.collectors.resource.immutables.ImmutableCollectionSetResource;
+import org.opennms.integration.api.v1.collectors.resource.immutables.ImmutableNodeResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -18,6 +26,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class UcsComputeCollector extends AbstractUcsServiceCollector {
     private final Map<UcsEnums.ClassId, Map<String,Set<UcsEnums.NamingClassId>>> collectionItemMap = new HashMap<>();
+
+    private static final Logger LOG = LoggerFactory.getLogger(UcsComputeCollector.class);
 
     public UcsComputeCollector(ClientManager clientManager, ConnectionManager connectionManager) {
         super(clientManager, connectionManager);
@@ -48,12 +58,38 @@ public class UcsComputeCollector extends AbstractUcsServiceCollector {
     }
 
     @Override
-    public CompletableFuture<CollectionSet> collect(CollectionRequest collectionRequest, Map<String, Object> map) {
+    public CompletableFuture<CollectionSet> collect(CollectionRequest request, Map<String, Object> attributes) {
+        Map<String, Set<UcsEnums.NamingClassId>> requestMap = new HashMap<>();
+        var dn = attributes.get(UcsUtils.UCS_DN_KEY).toString();
+        requestMap.put(
+                dn,
+                collectionItemMap.get(UcsEnums.ClassId.networkElement).get(UcsUtils.UCS_DN_KEY)
+        );
+        ApiClientService client;
         try {
-            ApiClientService client = getClient(map);
+            client = getClient(attributes);
         } catch (ApiException e) {
-            throw new RuntimeException(e);
+            LOG.error("collect: {}", requestMap, e );
+            return  CompletableFuture.failedFuture(e);
         }
+
+        final ImmutableNodeResource nodeResource = ImmutableNodeResource.newBuilder().setNodeId(request.getNodeId()).build();
+        UcsComputeStats stats;
+        try {
+            stats = client.getUcsComputeStats(requestMap);
+            client.disconnect();
+        } catch (ApiException e) {
+            LOG.error("collect: {}", requestMap, e );
+            return createFailedCollectionSet(nodeResource, Instant.now().toEpochMilli());
+        }
+
+        final ImmutableCollectionSetResource.Builder<NodeResource> networkElementAttrBuilder =
+                ImmutableCollectionSetResource.newBuilder(NodeResource.class).setResource(nodeResource);
+        final ImmutableCollectionSet.Builder resultBuilder = ImmutableCollectionSet.newBuilder();
+        resultBuilder.addCollectionSetResource(networkElementAttrBuilder.build());
+
+        //return CompletableFuture.completedFuture(resultBuilder.setStatus(CollectionSet.Status.SUCCEEDED)
+        //        .setTimestamp().build());
         return null;
     }
 }
